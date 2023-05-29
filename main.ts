280894b9312e8f4b87355a555cdf42fd9ca78916
angeelams <130452816+angeelams@users.noreply.github.com>
2023-05-29 09:18:46 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let pasos = 0
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(pasos * 0.68)
+})
+input.onButtonPressed(Button.A, function () {
+    pasos = 0
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    pasos = 0
+    basic.showNumber(0)
+})
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.X) > 1300) {
+        pasos += 1
+    }
 })
